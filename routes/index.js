c65a094dc3a4c91ecc01936d1ad61795c4770758
Daniel Alvarez <alvarez.daniel@gmail.com>
2020-08-29 12:07:41 -0300

    Countdown in days

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var launchTime = new Date("12/29/2019 10:00").getTime();
+var launchTime = new Date("12/29/2020 10:00").getTime();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var currentTime = new Date().getTime();
   var numberOfMilliseconds = parseInt(launchTime - currentTime);
-  res.render('index', { title: 'Countdown to Launch', countDown: numberOfMilliseconds });
+  var numberOfDays = parseInt(numberOfMilliseconds/(24 * 3600 * 1000));
+  res.render('index', { title: 'Countdown to Launch', countDown: numberOfDays });
 });
 
 module.exports = router;
