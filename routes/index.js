f72439540fd036e8b2bd2310c99bded30acaa92f
Daniel Alvarez <alvarez.daniel@gmail.com>
2020-08-29 11:36:03 -0300

    Add countdown

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var launchTime = new Date("12/29/2019 10:00").getTime();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var currentTime = new Date().getTime();
+  var numberOfMilliseconds = parseInt(launchTime - currentTime);
+  res.render('index', { title: 'Countdown to Launch', countDown: numberOfMilliseconds });
 });
 
 module.exports = router;
